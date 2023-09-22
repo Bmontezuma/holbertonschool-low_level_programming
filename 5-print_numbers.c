4734596c418c1840f220188cb088bb7e24c45e4d
root <brandon.montezuma@holbertonstudents.com>
2023-09-22 16:54:43 -0700

    Numbers

---
+++ 5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print all of the single digit numbers of the base for this one
+ * 10 starting from 0, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
