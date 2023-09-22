d325ceccd33b41037d8a755cfe84ee80f2b18fa2
root <brandon.montezuma@holbertonstudents.com>
2023-09-21 18:39:08 -0700

    Putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_alphabet - a function that will print the alphabet
+*print_alphabet - a function that print alphabet
 *it prints in lowercase
 *Return: no return
 */
@@ -9,7 +9,7 @@ void print_alphabet(void)
 int letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 {
-_putchar (letter);
+_iputchar (letter);
 }
 _putchar (10);
 }
