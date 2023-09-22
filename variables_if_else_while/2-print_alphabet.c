7501e16373446706bb00ad888f0e684ffb4ff8dc
root <brandon.montezuma@holbertonstudents.com>
2023-09-21 17:08:58 -0700

    PUTCHAR

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,20 @@
  * 2-print_alphabet.c
  * Brandon M. waz here
  */
-
 #include 'main.h'
+#include "main.h"
 /**
- * Write program that will print the alphabet in lowercase
- * it will print in lowercase and a new line following
- */
+*print_alphabet - function that print alphabet
+*it prints it in lowercase
+*/
+
+
 void print_alphabet(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-
-	_putchar('\n');
-
+int letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar (letter);
+}
+_putchar (10);
 }
