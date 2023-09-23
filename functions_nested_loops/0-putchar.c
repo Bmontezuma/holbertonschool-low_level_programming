2debbba9fd69a4ac21cd521451883142ccf059ab
root <brandon.montezuma@holbertonstudents.com>
2023-09-23 12:24:08 -0700

    _putchar1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * Brandon WAZ here!
+ * 0_putchar
+ * I still cant believe I made it this far.
+ */
+
+/**
+ * main - prints putchar
+ * Return: 0 (SUCCESS LORD VADER)
+ */
+
+int main(void)
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
