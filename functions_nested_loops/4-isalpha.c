82639bf757af2bf5505c8661276e552f64f88bfa
root <brandon.montezuma@holbertonstudents.com>
2023-09-24 17:39:12 -0700

    isalpha4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -3,8 +3,9 @@
 /**
  * _isalpha - Checks if a character is an alphabetic character.
  * @c: The character to be checked.
- *
+ * Doing something different to help remember.
  * Return: 1 if @c is an alphabetic character, 0 if not.
+ * Determins if letter is upper or lower case and then returns value.
  */
 int _isalpha(int c)
 {
