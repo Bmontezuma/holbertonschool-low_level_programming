c8bb22f3f9cd14a2d81c8a0788d3050bf1515775
root <brandon.montezuma@holbertonstudents.com>
2023-09-25 16:39:56 -0700

    absolute

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * absolute_value - Compute the absolute value of an integer.
+ * @n: The integer to find the absolute value of.
+ *
+ * Return: The absolute value of the integer.
+ */
+
+int absolute_value(int n)
+{
+if (n >= 0)
+{
+(return n);
+}
+(return -n);
+}
+
