ffb42c3dbacfe6769515276ec1183eef6b2a887c
root <brandon.montezuma@holbertonstudents.com>
2023-09-25 19:17:06 -0700

    abs

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,31 +1,19 @@
 #include "main.h"
 
 /**
- * _abs - Calculate the absolute value of a number.
- * @i: The number for which to find the absolute value.
- *
- * This function takes an integer 'i' as input and computes its absolute value.
- * If 'i' is greater than 0, it returns 'i'. If 'i' is less than 0, it returns
- * the positive counterpart of 'i'. If 'i' is already 0, it returns 0 since the
- * absolute value of 0 is also 0.
- *
- * @param i: The integer whose absolute value is to be determined.
- *
- * Return: The absolute value of the integer 'i'.
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to find the absolute value of.
+ * Return: The absolute value of n.
  */
-int _abs(int i)
+int _abs(int n)
 {
-    if (i > 0)
-    {
-        return i; 
-    }
-    else if (i < 0)
-    {
-        return -i; 
-    }
-    else
-    {
-        return 0; 
-    }
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
 }
 
