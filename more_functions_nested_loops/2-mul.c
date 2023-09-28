6489fd9e303f9700ddba4052843e2deff5795c6c
root <brandon.montezuma@holbertonstudents.com>
2023-09-27 18:00:24 -0700

    Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two numbers, just like Ash's chainsaw hand!
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
