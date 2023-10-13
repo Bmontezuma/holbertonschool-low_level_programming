b52a71fd9c51f982e69a8db98882825a04f92953
root <brandon.montezuma@holbertonstudents.com>
2023-10-13 16:49:07 -0700

    Args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: The number of arguments
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
+
