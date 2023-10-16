b4aad4532cb0404550b91e79135607554af53ab5
root <brandon.montezuma@holbertonstudents.com>
2023-10-16 15:36:11 -0700

    Sqrt recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -4,8 +4,9 @@
  * find_sqrt - Helper function to find the square root of a number.
  * @n: The input number.
  * @x: The current value to check.
+ *
+ * Return: The square root of 'n' if found, or -1 if not.
  */
-
 int find_sqrt(int n, int x)
 {
 	if (x * x == n)
@@ -18,12 +19,12 @@ int find_sqrt(int n, int x)
 /**
  * _sqrt_recursion - Find the square root of a number.
  * @n: The input number.
+ *
+ * Return: The square root of 'n' if found, or -1 if not.
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
 	return (find_sqrt(n, 0));
 }
-
--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,32 +1,63 @@
-#include "main.h"
-#include <math.h>
+#include <stdio.h>
 
 /**
- * is_prime_number - Checks if a number is prime.
+ * is_prime_helper - A helper function that recursively checks if a number is prime.
+ * @n: The number to be checked for primality.
+ * @divisor: The current divisor being tested.
  *
- * @n: The number to check for primality.
+ * Return: 1 if the number is prime, 0 otherwise.
+ */
+int is_prime_helper(int n, int divisor)
+{
+    if (n <= 1) {
+        return (0);
+    }
+    if (divisor == 1) {
+        return (1);
+    }
+    
+    if (n % divisor == 0) {
+        return (0);
+    }
+    
+    return is_prime_helper(n, divisor - 1);
+}
+
+/**
+ * is_prime_number - Check if an input integer is a prime number.
+ * @n: The integer to be checked.
  *
- * Return: 1 if the number is prime, 0 if it's not.
+ * Return: 1 if the number is prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-	int i;
+    if (n < 2) {
+        return (0);
+    }
+    
+    return is_prime_helper(n, n - 1);
+}
 
-	if (n <= 1)
-		return (0);
-	if (n <= 3)
-		return (1);
-	if (n % 2 == 0 || n % 3 == 0)
-		return (0);
+int main(void)
+{
+    int r;
 
-	i = 5;
-	while (i * i <= n)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-			return (0);
-		i += 6;
-	}
-
-	return (1);
+    r = is_prime_number(1);
+    printf("%d\n", r);
+    r = is_prime_number(1024);
+    printf("%d\n", r);
+    r = is_prime_number(16);
+    printf("%d\n", r);
+    r = is_prime_number(17);
+    printf("%d\n", r);
+    r = is_prime_number(25);
+    printf("%d\n", r);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
+    printf("%d\n", r);
+    return (0);
 }
 
