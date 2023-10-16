372aee144b55c2a0ebe21f0d3d1ccd8563367235
root <brandon.montezuma@holbertonstudents.com>
2023-10-16 16:25:10 -0700

    Is prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -17,12 +17,12 @@ int is_prime_recursive(int n, int divisor);
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-		return (0);
-	if (n == 2)
-		return (1);
+if (n <= 1)
+return (0);
+if (n == 2)
+return (1);
 
-	return (is_prime_recursive(n, 2));
+return (is_prime_recursive(n, 2));
 }
 
 /**
@@ -34,11 +34,10 @@ int is_prime_number(int n)
  */
 int is_prime_recursive(int n, int divisor)
 {
-	if (divisor * divisor > n)
-		return (1);
-	if (n % divisor == 0)
-		return (0);
+if (divisor * divisor > n)
+return (1);
+if (n % divisor == 0)
+return (0);
 
-	return (is_prime_recursive(n, divisor + 1));
+return (is_prime_recursive(n, divisor + 1));
 }
-
