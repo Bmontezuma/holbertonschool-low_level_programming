943f8d74fca1efaa8dfbb29e9a56cf195c5f22b8
root <brandon.montezuma@holbertonstudents.com>
2023-10-18 16:13:25 -0700

    Create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,25 +8,25 @@
  *
  * Return: Pointer to the new array, or NULL on failure.
  */
+
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-	{
-		return (NULL);
-	}
-
-	char *array = (char *)malloc(size);
-
-	if (array == NULL)
-	{
-		return (NULL);
-	}
-
-	for (unsigned int i = 0; i < size; i++)
-	{
-		array[i] = c;
-	}
-
-	return (array);
+if (size == 0)
+{
+return (NULL);
 }
 
+char *array = (char *)malloc(size);
+
+if (array == NULL)
+{
+return (NULL);
+}
+
+for (unsigned int i = 0; i < size; i++)
+{
+array[i] = c;
+}
+
+return (array);
+}
