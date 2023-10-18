94d074b0e16ee08a800652d67948e3bf47d3dcf3
root <brandon.montezuma@holbertonstudents.com>
2023-10-18 16:40:47 -0700

    create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,6 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *array;
+unsigned int i;
 
 if (size == 0)
 {
@@ -23,7 +24,6 @@ if (array == NULL)
 return (NULL);
 }
 
-unsigned int i;
 for (i = 0; i < size; i++)
 {
 array[i] = c;
