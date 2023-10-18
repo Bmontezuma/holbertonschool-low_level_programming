5b63cbba6b61f188d80a1920e7344d809bbfdfb8
root <brandon.montezuma@holbertonstudents.com>
2023-10-18 16:56:15 -0700

    Str_concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - Concatenates two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: A pointer to the concatenated string, or NULL on failure.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *concatenated;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	concatenated = (char *)malloc(strlen(s1) + strlen(s2) + 1);
+
+	if (concatenated == NULL)
+		return (NULL);
+
+	strcpy(concatenated, s1);
+	strcat(concatenated, s2);
+
+	return (concatenated);
+}
+
