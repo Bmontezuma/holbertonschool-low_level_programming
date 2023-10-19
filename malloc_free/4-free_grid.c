c1e833e197a89306113773bc8ae8a4eaed5a28a6
root <brandon.montezuma@holbertonstudents.com>
2023-10-18 18:21:35 -0700

    Free_grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,18 +5,19 @@
  * free_grid - Frees a 2-dimensional grid created by alloc_grid.
  * @grid: The 2D array of integers to be freed.
  * @height: The height of the grid.
- *
- * This function deallocates the memory used by the grid and its rows.
  */
 void free_grid(int **grid, int height)
 {
-	if (grid != NULL && height > 0)
-	{
-		for (int i = 0; i < height; i++)
-		{
-			free(grid[i]);
-		}
-		free(grid);
-	}
+if (grid != NULL)
+{
+for (int i = 0; i < height; i++)
+{
+if (grid[i] != NULL)
+{
+free(grid[i]);
+}
+}
+free(grid);
+}
 }
 
