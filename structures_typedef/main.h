aaa192b913a4f977bef499671406102206064cf7
root <brandon.montezuma@holbertonstudents.com>
2023-10-25 16:02:25 -0700

    Dog.h

--- structures_typedef/main.h
+++ structures_typedef/main.h
@@ -1,7 +1,6 @@
-#ifndef PUTCHAR
-#define PUTCHAR
+#ifndef DOG_H
+#define DOG_H
 
-dog.h
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
