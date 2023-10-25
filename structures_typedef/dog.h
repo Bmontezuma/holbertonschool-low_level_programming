cde46ad3f5c567960cc33287654ec0e43fa178f4
root <brandon.montezuma@holbertonstudents.com>
2023-10-25 15:39:43 -0700

    Dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct Dog - Represents information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+struct Dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
+
