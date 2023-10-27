73d4505bad2902542cb887d6962d20145858a127
root <brandon.montezuma@holbertonstudents.com>
2023-10-26 17:52:24 -0700

    Sum them all

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -10,17 +10,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+va_list args;
 int sum = 0;
+unsigned int i;
 
 if (n == 0)
 {
 return (0);
 }
 
-va_list args;
 va_start(args, n);
 
-unsigned int i;
 for (i = 0; i < n; i++)
 {
 sum += va_arg(args, int);
