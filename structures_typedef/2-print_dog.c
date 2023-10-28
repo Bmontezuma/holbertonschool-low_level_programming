fb4d2efc7c92cf0faa12613e61002f875033361a
root <brandon.montezuma@holbertonstudents.com>
2023-10-28 15:24:05 -0700

    Init_dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+{
+char *name;
+char *breed;
+int age;
+};
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL) 
+		return;
+
+    	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+    	printf("Breed: %s\n", (d->breed != NULL) ? d->breed : "(nil)");
+    	printf("Age: %d\n", d->age);
+}
+
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * struct dog - A structure to represent information about a dog.
+ *
+ * @name:  The name of the dog.
+ * @age:   The age of the dog.
+ * @owner: The owner's name.
+ */
 struct dog
 {
 char *name;
@@ -8,4 +15,9 @@ float age;
 char *owner;
 };
 
-#endif /* DOG_H */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
+
+
+
