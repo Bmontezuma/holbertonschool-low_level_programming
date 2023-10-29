59232417e5010e427217a06966f497bb8321d96c
root <brandon.montezuma@holbertonstudents.com>
2023-10-28 20:56:28 -0700

    New Dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -8,31 +8,31 @@
  * @age: Age of the dog
  * @owner: Pointer to the owner string
  *
- * Return: Pointer to the new dog variable, or NULL if allocation fails
+ * Return: Pointer to the new dog, or NULL if fails
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog = malloc(sizeof(dog_t));
-    if (new_dog != NULL)
-    {
-        char *new_name = malloc(strlen(name) + 1);
-        char *new_owner = malloc(strlen(owner) + 1);
-        if (new_name != NULL && new_owner != NULL)
-        {
-            strcpy(new_name, name);
-            strcpy(new_owner, owner);
-            new_dog->name = new_name;
-            new_dog->age = age;
-            new_dog->owner = new_owner;
-        }
-        else
-        {
-            free(new_name);
-            free(new_owner);
-            free(new_dog);
-            new_dog = NULL;
-        }
-    }
-    return (new_dog);
+dog_t *new_dog = malloc(sizeof(dog_t));
+if (new_dog != NULL)
+{
+char *new_name = malloc(strlen(name) + 1);
+char *new_owner = malloc(strlen(owner) + 1);
+if (new_name != NULL && new_owner != NULL)
+{
+strcpy(new_name, name);
+strcpy(new_owner, owner);
+new_dog->name = new_name;
+new_dog->age = age;
+new_dog->owner = new_owner;
+}
+else
+{
+free(new_name);
+free(new_owner);
+free(new_dog);
+new_dog = NULL;
+}
+}
+return (new_dog);
 }
 
