c8e0cd09f1d8a0c69571020771ee9e7a8040e0a5
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-11 06:09:48 -0800

    Add Node End

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new_node;
-listint_t *last_node = *head;
+listint_t *last_node;
 
 new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
@@ -28,6 +28,7 @@ if (*head == NULL)
 return (new_node);
 }
 
+last_node = *head;
 while (last_node->next != NULL)
 {
 last_node = last_node->next;
