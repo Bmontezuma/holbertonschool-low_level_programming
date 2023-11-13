baa6769083451fc4935795f81a2e7e2fb6b895c2
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-13 15:25:08 -0800

    Under The Snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,48 +1,37 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * create_file - Creates a file and writes text content to it.
- * @filename: The name of the file to create.
- * @text_content: A NULL-terminated string to write to the file.
+ * create_file - creates a file with specified content
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return -1;
+int fd, len, write_status;
 
-    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-    if (fd == -1)
-        return -1;
+if (filename == NULL)
+return (-1);
 
-    if (text_content != NULL)
-    {
-        ssize_t bytes_written = write(fd, text_content, _strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+return (-1);
 
-    close(fd);
-    return 1;
+if (text_content != NULL)
+{
+for (len = 0; text_content[len] != '\0'; len++)
+;
+
+write_status = write(fd, text_content, len);
+if (write_status == -1)
+{
+close(fd);
+return (-1);
+}
 }
 
-/**
- * _strlen - Calculates the length of a string.
- * @str: The input string.
- *
- * Return: The length of the string.
- */
-size_t _strlen(char *str)
-{
-    size_t len = 0;
-    while (str[len] != '\0')
-        len++;
-    return len;
+close(fd);
+return (1);
 }
 
