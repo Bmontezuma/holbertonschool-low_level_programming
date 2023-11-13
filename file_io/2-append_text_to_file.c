3f6ed836c4e7a0434c0a8b8210a937e3b10e11b4
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-13 15:31:35 -0800

    Speak Gently

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,48 +1,37 @@
 #include "main.h"
-#include <fcntl.h>
-#include <unistd.h>
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: The name of the file.
- * @text_content: The NULL-terminated string to add at the end of the file.
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of the file
+ * @text_content: NULL terminated string to add at the end of the file
  *
- * Return: 1 on success and -1 on failure.
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    if (filename == NULL)
-        return -1;
+	int fd, len, write_status;
 
-    int fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return -1;
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
-    {
-        ssize_t bytes_written = write(fd, text_content, _strlen(text_content));
-        if (bytes_written == -1)
-        {
-            close(fd);
-            return -1;
-        }
-    }
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-    close(fd);
-    return 1;
-}
+	if (text_content != NULL)
+	{
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
 
-/**
- * _strlen - Calculates the length of a string.
- * @str: The input string.
- *
- * Return: The length of the string.
- */
-size_t _strlen(char *str)
-{
-    size_t len = 0;
-    while (str[len] != '\0')
-        len++;
-    return len;
+	write_status = write(fd, text_content, len);
+	if (write_status == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	}
+
+		close(fd);
+		return (1);
 }
 
