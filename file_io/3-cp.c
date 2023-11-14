d2e77bfd5d3dc5184032775e843268f71819d82f
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-13 16:26:09 -0800

    Updated File

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,33 +6,39 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int main(void)
-{
-int fd_from, fd_to, read_status, write_status, close_status;
-char buffer[1024];
-const char *file_from = "test_folder/textfile_0";
-const char *file_to = "test_folder/textfile_0_copy_3";
+#include "main.h"
 
-fd_from = open(file_from, O_RDONLY);
-fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-
-if (fd_from == -1 || fd_to == -1)
+int main(int ac, char **av)
 {
-perror("Error");
-return (1);
+int f1, f2, r, w, c;
+char buffer[1024] = {0};
+
+if (ac != 3)
+{
+dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", *av);
+return (EXIT_FAILURE);
 }
 
-while ((read_status = read(fd_from, buffer, sizeof(buffer))) > 0 &&
-(write_status = write(fd_to, buffer, read_status)) == read_status);
+f1 = open(av[1], O_RDONLY);
+f2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-close_status = close(fd_from) | close(fd_to);
-
-if (close_status == -1)
+if (ac != 3 || f1 == -1 || f2 == -1)
 {
 perror("Error");
-return (1);
+return (EXIT_FAILURE);
 }
 
-return (0);
+while ((r = read(f1, buffer, 1024)) > 0 &&
+(w = write(f2, buffer, r)) == r);
+
+c = close(f1) | close(f2);
+
+if (c == -1)
+{
+perror("Error");
+return (EXIT_FAILURE);
+}
+
+return (EXIT_SUCCESS);
 }
 
