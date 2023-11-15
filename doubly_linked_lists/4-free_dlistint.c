1b4607c74f0244695ba83f11616e6a108e1c4751
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-15 06:00:19 -0800

    Free List

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+/* free_dlistint.c */
+
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - Frees a list.
+ * @head: Pointer to the head of the list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *temp;
+
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
+}
