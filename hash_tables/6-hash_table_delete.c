3bbed45dddacdc07012dc518fa3e14f7a3edd923
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-18 04:27:35 -0800

    Del HT

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: The hash table to delete.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+if (ht == NULL)
+return;
+
+for (unsigned long int i = 0; i < ht->size; ++i)
+{
+hash_node_t *current = ht->array[i];
+while (current != NULL)
+{
+hash_node_t *next = current->next;
+free(current->key);
+free(current->value);
+free(current);
+current = next;
+}
+}
+
+free(ht->array);
+free(ht);
+}
