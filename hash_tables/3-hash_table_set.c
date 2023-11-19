7019c2e239b6dfc280923f5fe8da51612610651e
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-18 20:09:24 -0800

    Updated Files

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 /**
- * hash_table_set - Adds an element to the table.
- * @ht: The hash table to add/update the key/value to
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add/update the key/value pair
  * @key: The key string.
  * @value: The value associated with the key.
  *
@@ -12,47 +12,33 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node;
+    unsigned long int index;
+    hash_node_t *new_node;
 
-	if (!ht || !key || !*key)
-		return (0);
+    if (!ht || !key || !*key)
+        return (0);
 
-	index = key_index((const unsigned char *)key, ht->size);
+    index = key_index((const unsigned char *)key, ht->size);
 
-	new_node = malloc(sizeof(hash_node_t));
+    new_node = malloc(sizeof(hash_node_t));
 
-	if (!new_node)
-		return (0);
+    if (!new_node)
+        return (0);
 
-	new_node->key = malloc(strlen(key) + 1);/* key_index.c */
+    new_node->key = strdup(key);
+    new_node->value = (new_node->key) ? strdup(value) : NULL;
 
-#include "hash_tables.h"
+    if (!new_node->key || !new_node->value)
+    {
+        free(new_node->key);
+        free(new_node->value);
+        free(new_node);
+        return (0);
+    }
 
-/**
- * key_index - Gives the index of a key in a hash table.
- * @key: The key string
- * @size: The size of the hash table array
- *
- * Return: Index at which the key should be stored in the array.
- */
-unsigned long int key_index(const unsigned char *key, unsigned long int size)
-{
-        return (hash_djb2(key) % size);
-}
-	new_node->value = (new_node->key) ? strdup(value) : NULL;
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
 
-	if (!new_node->key || !new_node->value)
-	{
-	free(new_node->key);
-	free(new_node->value);
-	free(new_node);
-		return (0);
-	}
-
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
-
-		return (1);
+    return (1);
 }
 
