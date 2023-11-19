8120a65783cd7574f5dcc60f9b70154f435f81de
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-18 19:38:52 -0800

    Updated Files

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new_node)
 		return (0);
 
-	new_node->key = strdup(key);
+	new_node->key = malloc(strlen(key) + 1);
 	new_node->value = (new_node->key) ? strdup(value) : NULL;
 
 	if (!new_node->key || !new_node->value)
