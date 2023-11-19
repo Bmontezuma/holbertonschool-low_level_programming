0896a9b227548d29ab1c362247151b12230dcf0a
Bmontezuma <brandon.montezuma@holbertonstudents.com>
2023-11-19 05:17:41 -0800

    Updated

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 #include <stdio.h>
-#include <string.h>
+
 /**
  * hash_table_print - Prints a hash table.
  * @ht: The hash table to print.
@@ -15,6 +15,7 @@ void hash_table_print(const hash_table_t *ht)
     printf("{");
 
     unsigned long int i;
+
     for (i = 0; i < ht->size; ++i)
     {
         hash_node_t *current = ht->array[i];
